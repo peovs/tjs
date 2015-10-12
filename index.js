52edd6e8f48e2d3c8f165c790cd42366953186f1
thorvaldj <thorvaldj@gmail.com>
2015-10-12 12:38:29 +0200

    changing portno again

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var express = require('express');
+
+var port = (process.env.PORT || '3000');
+ 
 var app = express();
 app.get('/',function(req,res){
 	
 	res.send(siHei(req));})
-app.listen(80);
-writeStatus(80);
+app.listen(port);
+writeStatus(port);
 
 function siHei(req){
 	console.log("request:"+req.body);
