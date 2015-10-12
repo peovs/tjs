c0d5d5fa1c7b5b3b18b72af13ab64c6567ef754e
unknown <THORVJO@LT-879402.statoil.net>
2015-10-12 14:53:27 +0200

    rerouting message

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var store = require('./routes/store');
-var message = require('./routes/store-message');
+
 var app = express();
 
 // view engine setup
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/store', store);
-app.use('/store/message', message);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/store.js
+++ routes/store.js
@@ -7,4 +7,12 @@ router.get('/', function(req, res, next) {
 
 
 
+router.route('/message')
+  .post(function(req, res, next) {
+    console.log(req.body.message);
+    res.send('message :' + req.body.message );
+   
+  });
+
+
 module.exports = router;
