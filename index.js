68011577ff9fdb0d51bbccb3ccf13d146b5947b2
thorvaldj <thorvaldj@gmail.com>
2015-10-12 12:29:09 +0200

    updating portno

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
-app.get('/',function(req,res){res.send(siHei());})
-app.listen(3000);
-
-
-function siHei(){
+app.get('/',function(req,res){
 	
+	res.send(siHei(req));})
+app.listen(80);
+writeStatus(80);
+
+function siHei(req){
+	console.log("request:"+req.body);
 	return 'hei';
+}
+
+function 	writeStatus(portNo){
+	console.log("application is started on port:"+portNo);
+	
 }
