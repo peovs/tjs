3fa75cf2895c951d974190f53233c359523c06bc
unknown <THORVJO@LT-879402.statoil.net>
2015-10-12 14:19:54 +0200

    added store form and message

--- routes/store.js
+++ routes/store.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-
-
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('store', { title: 'store' });
 });
+
+
 
 module.exports = router;
